f3c512afb4fc81885317e335534e05668694a475
abdallahabusedo <abdallahsedo@hotmail.com>
2023-07-01 00:38:50 +0300

    try again cors library

--- index.js
+++ index.js
@@ -12,7 +12,20 @@ const port = 3001;
 const ObjectID = require("mongodb").ObjectId;
 dbConnect();
 
+/** CORS setting with OPTIONS pre-flight handling */
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, accept, access-control-allow-origin"
+//   );
+
+//   if ("OPTIONS" == req.method) res.send(200);
+//   else next();
+// });
 app.use(cors());
+app.options("*", cors());
 // Curb Cores Error by adding a header here
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");

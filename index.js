3f2ce99a0cbeb5ac632468a5a0d180177ca2061a
abdallahabusedo <abdallahsedo@hotmail.com>
2023-07-01 00:35:04 +0300

    try with cors library

--- index.js
+++ index.js
@@ -11,21 +11,8 @@ const User = require("./db/userModel");
 const port = 3001;
 const ObjectID = require("mongodb").ObjectId;
 dbConnect();
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.get("Origin") || "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header("Access-Control-Expose-Headers", "Content-Length");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Accept, Authorization, Content-Type, X-Requested-With, Range"
-  );
-  if (req.method === "OPTIONS") {
-    return res.send(200);
-  } else {
-    return next();
-  }
-});
+
+app.use(cors());
 // Curb Cores Error by adding a header here
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
